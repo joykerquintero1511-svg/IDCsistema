d2dcb1477e8d75310d26caf054d3cb093bb64a2e
Katherine Perdomo <katherinejp10@gmail.com>
2026-05-06 13:44:10 -0400

    use require_once

--- estudiantes/listar.php
+++ estudiantes/listar.php
@@ -1,12 +1,10 @@
 <?php
-    include'../conexion.php'; // para la conexion con la BD
-    include'../nav.php'; // incluye menu de navegacion,y asi el menu aparece en todas las paginas
-
-
+    require_once '../conexion.php'; // conexión a la BD (más seguro que include)
+    require_once '../nav.php';     // menú de navegación
 ?>
 
     <!DOCTYPE html>
-    <html lang="en">
+    <html lang="es"> <!-- Coloco "es" xq la pag estara en español, "en"significa ingles
     <head>
         <meta charset="UTF-8"> <!--	Asegura que los textos con tildes y la ñ se vean bien-->
         <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!--Hace que la página sea responsive (se adapte a móviles)-->
@@ -32,15 +30,23 @@
             </tr>    
          </thead>
          <tbody> <!-- Cuerpo de la tabla (donde van los datos)-->
+
           <?php
-          $sql="SELECT * FROM estudiantes";  // Consulta SQL para seleccionar todos los estudiantes
+          $sql = "SELECT * FROM estudiantes";  // Consulta SQL para seleccionar todos los estudiantes
+
+          global $conexion;  
+ /* Cree esta  global $conexion; para decirle 
+ “hey PHP, esa variable que viene de otro archivo, úsala aquí también */
+
+
         $resultado = mysqli_query($conexion,$sql);   // Ejecuta la consulta en la base de datos
+  
+ // VALIDACIÓN DE ERROR 
+
+        if (!$resultado) {
+             die("Error en la consulta: " . mysqli_error($conexion));
+  }
     
-         /* $conexion esta subrayado pero NO ES ERROR 
-        solo q la variable viene del archivo conexion.php 
-        (la extensión Intelephense)piensa q esa variable
-        no est adefinida pero El código funciona perfectamente."
-    */
         while ($fila= mysqli_fetch_assoc($resultado)){ // Recorre cada fila de resultados obtenidos
            
             echo "<tr>"; // Imprime una celda con el dato del estudiante
