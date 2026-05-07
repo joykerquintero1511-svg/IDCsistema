de31894a410f734b1552ec1e8883f959d998dc6e
Katherine Perdomo <katherinejp10@gmail.com>
2026-05-07 14:44:09 -0400

    quite label

--- estudiantes/agregar.php
+++ estudiantes/agregar.php
@@ -3,7 +3,7 @@ require_once '../conexion.php';
 
 $conexion = $conexion ?? null; // el xq d esta variable esta en la carpeta de listar.php
 
-$nombre = $apellido = $email = $telefono = $nivel = "";
+$nombre = $apellido =$cedula =$direccion = $email = $telefono = $nivel_academico = $estado_civil = $nivel_instruccion = $fecha_registro = "";
 $error = "";
 $exito = "";
 
@@ -11,17 +11,23 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     /* $_POST['nombre'] Toma el valor que el usuario escribió en el campo name="nombre"
     trim(...)	Elimina espacios en blanco al inicio y al final del texto */
 
+# Recibir datos 
     $nombre = trim($_POST['nombre']); 
     $apellido = trim($_POST['apellido']);
+    $cedula = trim($_POST['cedula']);
+    $direccion = trim($_POST ['direccion']);
     $email = trim($_POST['email']);
     $telefono = trim($_POST['telefono']);
-    $nivel = trim($_POST['nivel_academico']);
+    $nivel_academico = trim($_POST['nivel_academico']);
+    $estado_civil = trim($_POST['estado_civil']);
+    $nivel_instruccion = trim($_POST['nivel_instruccion']);
+    $fecha_registro = trim($_POST['fecha_registro']);
 
     if (empty($nombre) || empty($apellido)) {   // (empty) esta explicado en registrar_usuario.php
         $error = "Nombre y Apellido son obligatorios.";
     } else {
-        $sql = "INSERT INTO estudiantes (nombre, apellido, email, telefono, nivel_academico) 
-                VALUES ('$nombre', '$apellido', '$email', '$telefono', '$nivel')";
+        $sql = "INSERT INTO estudiantes (nombre, apellido,cedula, direccion, email, telefono, nivel_academico, estado_civil,nivel_instruccion, fecha_registro ) 
+                VALUES ('$nombre', '$apellido', '$cedula','$direccion','$email', '$telefono', '$nivel_academico', '$estado_civil', '$nivel_instruccion','$fecha_registro')";
 
     # Esta es la q cree en conexion.php 
         if (ejecutarConsulta($conexion, $sql)) {
@@ -56,20 +62,28 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 
     <form method="POST"> <!-- El formulario envía los datos ocultos (seguro) con POST-->
-        <label>Nombre:</label><br>
-        <input type="text" name="nombre" value="<?php echo htmlspecialchars($nombre); ?>" required><br><br>
+        <input type="text" name="nombre" placeholder="Nombres" value="<?php echo htmlspecialchars($nombre); ?>" required><br><br>
+        <input type="text" name="apellido" placeholder="Apellido" value="<?php echo htmlspecialchars($apellido); ?>" required ><br><br>
+        <input type= "text" name="cedula" placeholder="Cedula" value="<?php echo htmlspecialchars($cedula);?>"required><br><br>
+        <input type="text" name= "direccion" placeholder="Dirección" value="<?php echo htmlspecialchars($direccion);?>"required><br><br>
+        <input type="text" name="email" placeholder="Email" value="<?php echo htmlspecialchars($email); ?>"><br><br> 
+        <input type="text" name="telefono" placeholder="Telefono" value="<?php echo htmlspecialchars($telefono);?>"required><br><br>
+       <input type="text" name="nivel_academico" placeholder="Nivel Académico (Ej: 1A, 2B, 3C)" value="<?php echo htmlspecialchars($nivel_academico); ?>" required><br><br>
+        <input type ="text" name="estado_civil" placeholder="Estado_civil" value="<?php echo htmlspecialchars($estado_civil);?>"required><br><br>
+        <input type="date" name="fecha_registro" placeholder="Fecha de Registro" value="<?php echo htmlspecialchars($fecha_registro); ?>"><br><br>       
 
-        <label>Apellido:</label><br>
-        <input type="text" name="apellido" value="<?php echo htmlspecialchars($apellido); ?>" required><br><br>
+        <select name="nivel_instruccion">
+            <option value="">Seleccione</option>
+            <option value="Primaria">Primaria</option>
+            <option value="Secundaria">Secundaria</option>
+            <option value="Bachiller">Bachiller</option> 
+            <option value="Técnico">Técnico</option>
+            <option value="Postgrado">Postgrado</option>
 
-        <label>Email:</label><br>
-        <input type="email" name="email" value="<?php echo htmlspecialchars($email); ?>"><br><br>
+        </select> <br><br>
 
-        <label>Teléfono:</label><br>
-        <input type="text" name="telefono" value="<?php echo htmlspecialchars($telefono); ?>"><br><br>
 
-        <label>Nivel Académico:</label><br>
-        <input type="text" name="nivel_academico" value="<?php echo htmlspecialchars($nivel); ?>"><br><br>
+
 
         <button type="submit">Guardar Estudiante</button>
         <a href="listar.php">Cancelar</a>
