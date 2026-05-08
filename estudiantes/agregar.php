fceecc551f3f8ea4a596ce0a11d25c1ac760c558
Katherine Perdomo <katherinejp10@gmail.com>
2026-05-08 13:39:00 -0400

    status

--- estudiantes/agregar.php
+++ estudiantes/agregar.php
@@ -78,11 +78,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <input class="formulario-input" type="text" name="email" placeholder="Email" value="<?php echo htmlspecialchars($email); ?>"><br><br> 
         <input class="formulario-input" type="text" name="telefono" placeholder="Telefono" value="<?php echo htmlspecialchars($telefono);?>"required><br><br>
        <input class="formulario-input" type="text" name="nivel_academico" placeholder="Nivel Académico (Ej: 1A, 2B, 3C)" value="<?php echo htmlspecialchars($nivel_academico); ?>" required><br><br>
-        <select name="status">
-        <option value="Activo" <?php echo ($status == 'Activo') ? 'selected' : ''; ?>>Activo</option>
-        <option value="Inactivo" <?php echo ($status == 'Inactivo') ? 'selected' : ''; ?>>Inactivo</option>
-        <option value="Egresado" <?php echo ($status == 'Egresado') ? 'selected' : ''; ?>>Egresado</option>
-        <option value="Inhabilitado" <?php echo ($status == 'Inhabilitado') ? 'selected' : ''; ?>>Inhabilitado</option>
+       <select class="formulario-input" type="text" name="status" placeholder="status">
+    <option value="" disabled selected style="color: gray;">Estado</option>
+    <option value="Activo" <?php echo ($status == 'Activo') ? 'selected' : ''; ?>>Activo</option>
+    <option value="Inactivo" <?php echo ($status == 'Inactivo') ? 'selected' : ''; ?>>Inactivo</option>
+    <option value="Egresado" <?php echo ($status == 'Egresado') ? 'selected' : ''; ?>>Egresado</option>
+    <option value="Inhabilitado" <?php echo ($status == 'Inhabilitado') ? 'selected' : ''; ?>>Inhabilitado</option>
+
     </select><br><br>
         <input class="formulario-input" type="date" name="fecha_registro" placeholder="Fecha de Registro" value="<?php echo htmlspecialchars($fecha_registro); ?>"><br><br>       
 
