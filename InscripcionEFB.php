fa71c33135401cba05e93f88a53eaf380258c009
joykerquintero1511 <joykerquintero1511@gmail.com>
2026-05-08 15:16:27 -0400

    letras pequeñas eliminadas

--- InscripcionEFB.php
+++ InscripcionEFB.php
@@ -34,7 +34,7 @@ include("Niveles.php");
                 
                 <input class="formulario-input" type="text" id="telefono" name="telefono" placeholder="Número de Teléfono" required><br>
                 <input class="formulario-input" type="password" name="contraseña" placeholder="Contraseña (mínimo 6 caracteres)" required>
-                    <small style="color: gray;">La contraseña debe tener al menos 6 caracteres</small><br><br>
+                    
                 
                 <select class="inscripciones-input" name="nivel_academico">
                 <option value="">Seleccione el nivel</option>
